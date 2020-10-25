d6468a0f3d2863b72e7587aaa6f68f9c36644d81
Jasom Park <zeipar@gmail.com>
2020-10-25 00:46:15 -0700

    Created a Get decorater that uses @param decorator.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    @Get()
+    getAll() {
+        return "This will return something";
+    }
+
+    @Get("/:id")
+    getOne(@Param("id") movieId: string) {
+        return `This will return one item with id: ${movieId}`;
+    }
+}
