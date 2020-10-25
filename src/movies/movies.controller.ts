5e8ab239aa26b148bfd840b1c86ee6b873e2cd37
Jasom Park <zeipar@gmail.com>
2020-10-25 00:49:28 -0700

    Created Post, Delete and Patch endpoints and tested.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -11,4 +11,20 @@ export class MoviesController {
     getOne(@Param("id") movieId: string) {
         return `This will return one item with id: ${movieId}`;
     }
+
+    @Post()
+    create() {
+        return `This will create a movie`;
+    }
+
+    @Delete("/:id")
+    remove(@Param("id") movieId: string) {
+        return `This will delete a movie with id: ${movieId}`;
+    }
+
+    @Patch("/:id")
+    patch(@Param("id") movieId: string) {
+        return `This will patch a movie with id: ${movieId}`;
+    }
+
 }
