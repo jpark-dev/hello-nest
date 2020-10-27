aaeba0c672e71dfc797836cf123d7b5b23452cd6
Jasom Park <zeipar@gmail.com>
2020-10-26 23:02:02 -0700

    Added 'transform' option in ValidationPipe which automatically transforms the variable type. Updated controller/service to get rid of type coercion '+id'.

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -11,15 +11,15 @@ export class MoviesService {
         return this.movies;
     }
 
-    getOne(id:string): Movie {
-        const movie = this.movies.find(movie => movie.id === +id);
+    getOne(id:number): Movie {
+        const movie = this.movies.find(movie => movie.id === id);
         if(!movie) {
             throw new NotFoundException(`Movie with ID: ${id} not found.`);
         }
         return movie;
     }
 
-    deleteOne(id: string){
+    deleteOne(id: number){
         this.getOne(id);
         this.movies = this.movies.filter(movie => movie.id !== +id);
     }
@@ -31,7 +31,7 @@ export class MoviesService {
         })
     }
 
-    update(id: string, updateData) {
+    update(id: number, updateData) {
         const movie = this.getOne(id);
         this.deleteOne(id);
         this.movies.push({ ...movie, ...updateData});
