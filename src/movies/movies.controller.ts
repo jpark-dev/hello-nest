98213807379d9b904970c5e54346ca2196b7e23a
Jason Park <zeipar@gmail.com>
2020-11-01 23:02:21 -0800

    Updated controller/service to use the newly created movie update dto.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,44 +1,49 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
 
-    constructor(private readonly moviesService: MoviesService) {
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
 
-    }
+  @Get('search')
+  search(@Query('year') searchInYear: string) {
+    return `Searching for a movie made after: ${searchInYear}`;
+  }
 
-    @Get()
-    getAll(): Movie[] {
-        return this.moviesService.getAll();
-    }
+  @Get(':id')
+  getOne(@Param('id') movieId: number) {
+    return this.moviesService.getOne(movieId);
+  }
 
-    @Get("search")
-    search(@Query('year') searchInYear:string) {
-        return `Searching for a movie made after: ${searchInYear}`;
-    }
-    
-    @Get(":id")
-    getOne(@Param("id") movieId: number) {
-        return this.moviesService.getOne(movieId);
-    }
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.createOne(movieData);
+  }
 
-    @Post()
-    create(@Body() movieData: CreateMovieDto) {
-        return this.moviesService.createOne(movieData);
-    }
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    @Delete(":id")
-    remove(@Param("id") movieId: number) {
-        return this.moviesService.deleteOne(movieId);
-    }
-
-    @Patch(":id")
-    patch(@Param("id") movieId: number, @Body() updateData) {
-        return this.moviesService.update(movieId, updateData);
-    }
-
-
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,39 +1,40 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
-    private movies: Movie[] = [];
+  private movies: Movie[] = [];
 
-    getAll(): Movie[] {
-        // query to be used for real DB
-        return this.movies;
-    }
+  getAll(): Movie[] {
+    // query to be used for real DB
+    return this.movies;
+  }
 
-    getOne(id:number): Movie {
-        const movie = this.movies.find(movie => movie.id === id);
-        if(!movie) {
-            throw new NotFoundException(`Movie with ID: ${id} not found.`);
-        }
-        return movie;
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID: ${id} not found.`);
     }
+    return movie;
+  }
 
-    deleteOne(id: number){
-        this.getOne(id);
-        this.movies = this.movies.filter(movie => movie.id !== +id);
-    }
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter(movie => movie.id !== +id);
+  }
 
-    createOne(movieData: CreateMovieDto) {
-        this.movies.push({
-            id: this.movies.length + 1,
-            ...movieData
-        })
-    }
+  createOne(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
 
-    update(id: number, updateData) {
-        const movie = this.getOne(id);
-        this.deleteOne(id);
-        this.movies.push({ ...movie, ...updateData});
-    }
+  update(id: number, updateData: UpdateMovieDto) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
 }
