16ac91f021c51cc3b4cc82375b96fa05500936c2
Jason Park <zeipar@gmail.com>
2020-11-01 23:38:50 -0800

    Used PartialType() instead of copy&paste idential dto from CreateMovieDto.

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,12 +1,15 @@
-import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-  @IsString()
-  readonly title?: string;
+// export class UpdateMovieDto {
+//   @IsString()
+//   readonly title?: string;
+//
+//   @IsNumber()
+//   readonly year?: number;
+//
+//   @IsString({ each: true })
+//   readonly genres?: string[];
+// }
 
-  @IsNumber()
-  readonly year?: number;
-
-  @IsString({ each: true })
-  readonly genres?: string[];
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
