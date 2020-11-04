9679e5ea4486d731625319ee274da86a0d6a791c
Jason Park <zeipar@gmail.com>
2020-11-03 23:51:41 -0800

    Updated e2e testing for '/movies (GET)'.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,11 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Welcome to my Movie API');
   });
+
+  it('/movies (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/movies')
+      .expect(200)
+      .expect([]);
+  });
 });
