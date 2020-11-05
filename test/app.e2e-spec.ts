be97b9791e86e5f68e34c8319c228e62cdd036a5
Jason Park <zeipar@gmail.com>
2020-11-04 22:28:18 -0800

    Added tests to check patch, delete, and post 400.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -63,6 +63,28 @@ describe('AppController (e2e)', () => {
           .get('/movies/1')
           .expect(200);
       });
+      it('PATCH', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/1')
+          .send({ title: 'new Test new new!' })
+          .expect(200);
+      });
+      it('DELETE', () => {
+        return request(app.getHttpServer())
+          .delete('/movies/1')
+          .expect(200);
+      });
+      it('POST 400', () => {
+        return request(app.getHttpServer())
+          .post('/movies')
+          .send({
+            title: 'Test',
+            year: 2000,
+            genres: ['test'],
+            other: 'thing',
+          })
+          .expect(400);
+      });
     });
   });
 });
